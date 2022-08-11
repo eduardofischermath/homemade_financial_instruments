b4fc9629bd6b9ed9c7f960cd96c07ac66097e8f8
Eduardo Fischer <eduardofischermath@gmail.com>
2022-08-11 16:52:35 -0300

    Wrote functions to uniformize user input treatment in demo functions.

--- src py/demos.py
+++ src py/demos.py
@@ -18,18 +18,55 @@ elif sys_version_info.major == 3:
 else:
   raise ValueError('Only works with Python 2 and 3.')
 
+def detect_initial(string, desired_first_letter, be_case_sensitive = False,
+    desired_return_if_empty_string = False):
+  """Detects whether the first letter of a string is the desired one."""
+  if string:
+    if be_case_sensitive:
+      is_desired = string[0] == desired_first_letter
+    else:
+      is_desired = string[0].lower() == desired_first_letter.lower()
+    return is_desired
+  else:
+    return desired_return_if_empty_string
+
+def capture_purify_ehxibit_input(message, type_to_convert_to = None,
+    pre_conversion_purifying_formula = None, post_conversion_purifying_formula = None,
+    public_variable_name = None, request_exhibition = False):
+  r"""
+  Takes a raw input and makes it into the desirable type, returning it.
+  
+  Can make it go through formulas ("purification"), can also show it to the user.
+  """
+  input_string = best_input(message)
+  if pre_conversion_purifying_formula:
+    input_string = pre_conversion_purifying_formula(input_string)
+  if type_to_convert_to:
+    input_object = type_to_convert_to(input_string)
+  if post_conversion_purifying_formula:
+    input_object = post_conversion_purifying_formula(input_object)
+  if request_exhibition:
+    if public_variable_name is None:
+      string_before_object = ''
+    else:
+      string_before_object = f'{public_variable_name}: '
+    print(string_before_object + str(input_object))
+  return input_object
+
 def all_demos():
   """Provides envelope for all demonstration functions."""
   should_keep_going = True
   while should_keep_going:
-    evaluate_option_using_binary_tree_demo()
-    should_keep_going_raw = best_input('Wish to continue? [Y]es or [N]o.\n')
-    if should_keep_going_raw[0].lower() == 'y':
-      pass
-    else:
-      should_keep_going = False
-    
-def evaluate__vanilla_option_using_binary_tree_demo():
+    evaluate_vanilla_option_using_binary_tree_demo
+    should_keep_going = capture_purify_ehxibit_input(
+        message = 'Wish to continue? [Y]es or [N]o.\n',
+        type_to_convert_to = bool,
+        pre_conversion_purifying_formula = lambda s: detect_initial(s, 'y'),
+        post_conversion_purifying_formula = None,
+        public_variable_name = None,
+        request_exhibition = False)
+
+def evaluate_vanilla_option_using_binary_tree_demo():
   """Computes value of vanilla option with user-provided information"""
   pass
 
