496746b79aa3363c4b148594c44b3df64374ce74
Eduardo Fischer <eduardofischermath@gmail.com>
2022-09-10 15:20:44 -0300

    (issue0006py) Better documentation of DictionaryArgumentProcessor, some classes more adapted to it.

--- src py/formulas.py
+++ src py/formulas.py
@@ -57,35 +57,66 @@ class FormulaOnDictionaries():
   dictionary for correct extraction for are given at instantiation.
   """
   
-  def __init__(self, dict_for_argument_extraction, inner_formula = None,
+  def __init__(self, dict_for_argument_processing, inner_formula = None,
       inner_function = None, argument_handler = None):
     if inner_formula is None:
       inner_formula = Formula(inner_function, argument_handler)
     self.inner_formula = inner_formula
-    self.dict_for_argument_extraction = dict_for_argument_extraction
+    # Can give either the dictionary or the DictionaryArgumentProcessor
+    # If a dict is given, then complete_args_with_nones will be False
+    if isinstance(dict_for_argument_processing, DictionaryArgumentProcessor):
+      self.dict_processor = dict_for_argument_processing
+    elif isinstance(dict_for_argument_processing, dict):
+      self.dict_processor = DictionaryArgumentProcessor(
+          dict_for_argument_processing = dict_for_argument_processing,
+          complete_args_with_nones = False)
+    else:
+      raise TypeError('dict_for_argument_processing must be either a dict'\
+          'or a DictionaryArgumentProcessor')
 
   def call(self, *args, **kwargs):
     """Executes the inner formula and therefore the inner function"""
-    for key in dict_for_argument_extraction:
-      pass
-    return self.inner_formula.call(**computed_kwargs)
+    new_args, new_kwargs = self.dict_processor.process(args, kwargs)
+    return self.inner_formula.call(*new_args, **new_kwargs)
 
 class DictionaryArgumentProcessor():
   r"""
-  Transforms non-keyword and keyword arguments (that is, a tuple and a
-  dictionary) into new non-keyword and keyword arguments using a specific
-  dictionary given at its initiation.
+  Transforms non-keyword and keyword arguments into new non-keyword and
+  keyword arguments using a specific dictionary given at its initiation.
   
-  In more detail, the variable dict_for_argument_extraction is a dict
-  whose keys are strings and whose values are length three tuples. The
-  first of these items is either 'arg' or 'kwarg'. The second is a nonnegative
-  integer if the first item is 'arg', and a string otherwise. The third
-  is always a string. This is done so that, in the call method called
-  with non-keyword arguments args and keyword arguments kwargs, the inner
-  formula is called with the a dict of keyword arguments whose keys are
-  the keys of dict_for_argument_extraction and whose values are
-  args[value[1]][value[2]] if value[0] is 'arg' and
-  kwargs[value[1]][value[2]] if value[0] is 'kwargs'.
+  The transformation is done on a tuple and a dict which are interpreted
+  as non-keyword and keyword arguments of a function or method call.
+  This interpretation is optional.
+  
+  In more detail, the attribute dict_for_argument_processing is a dict
+  whose keys are either nonnegative integers (giving rise to non-keyword
+  arguments) or strings (giving rise to keyword arguments) and whose values
+  are length three tuples. The first of these items is either 'arg' or
+  'kwarg'. The second is a nonnegative integer if the first item is 'arg',
+  and a string otherwise. The third is always a string.
+  
+  This is done so that the transform method, which receives a tuple named
+  args and a dict named kwargs, produces a tuple named new_args and
+  a dictionary named new_kwargs. For each key in dict_for_argument_processing,
+  there will be a new_value which will be either (here value is the
+  corresponding value) args[value[1]][value[2]] if value[0] is 'arg'
+  and kwargs[value[1]][value[2]] if value[0] is 'kwargs', and this new_value
+  will either join new_args if the key is an nonnegative integer in which
+  case new_value will be new_args[key], or join new_kwargs if key is a string,
+  in which case new_value will be new_kwargs[key].
+  
+  The behavior of the formation of new_tuple is determined by the Boolean
+  attribute complete_args_with_nones. In True, if the numerical keys used
+  to derive the items of new_args don't complete a Python range from 0
+  to their maximum, then the remaining positions will be filled with None.
+  If False, an error will be raised.
   """
   
-  pass
+  def __init__(dict_for_argument_processing, complete_args_with_nones = False):
+    self.dict_for_argument_processing = dict_for_argument_processing
+    self.allow_incomplete_tuple = allow_incomplete_tuple
+    
+  def transform(args, kwargs):
+    # Note: sometimes what is done is called transformation, sometimes processing
+    ### WORK HERE ###
+    return (new_args, new_kwargs)
