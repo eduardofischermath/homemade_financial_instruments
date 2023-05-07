f5196fd1ce2f6b112e949768b11396d3b6732eb2
Eduardo Fischer <eduardofischermath@gmail.com>
2023-05-07 11:19:34 -0300

    (issue0018py) Wrote methods for forming a box of characters from a dict.

--- src py/utilities.py
+++ src py/utilities.py
@@ -37,8 +37,9 @@ class StringManagement():
     Centers a string into a specific length.
     
     If allow_longer is True, this acts exactly as str.center, so if the
-    string is longer then it will return unaltered. Otherwise, it will
-    preserve only the initial characters.
+    string is longer then it will return unaltered. Otherwise, if
+    allow_longer is Falsen, it will preserve only the initial characters
+    of a longer string.
     """
     if not allow_longer and len(string) > length:
       string = string[0:length]
@@ -73,27 +74,76 @@ class StringManagement():
       also_print_keys = False, return_as_string = False):
     r"""
     Given a dictionary an a list of keys_to_print, produces a list of
-    strings for the values corresponding to the keys.
+    strings for the values corresponding to the keys. (It is returned,
+    not printed, despite the method's name.)
 
     If keys_to_print is None, it will take all dict keys in alphabetical
     order.
     
     If also_print_keys is True, the resulting string will be
-    'str(key):str(value)'. Otherwise, it is simply str(value)
+    'str(key):str(value)'. Otherwise, it is simply 'str(value)'
     
     If return_as_string is True, it instead performs a joint of the lines
-    into a single string with separator \n.
+    into a single string with separator '\n'.
     """
-    pass
-        
+    if keys_to_print is None:
+      keys_to_print = list(dictionary)
+      keys_to_print.sort(key = lambda x: str(x))
+    list_of_lines = []
+    for key in keys_to_print:
+      if key not in dictionary:
+        raise KeyError('Key not in dictionary')
+      value = dictionary[key]
+      if also_print_keys:
+        line = f'{key}:{value}'
+      else:
+        line = f'{value}'
+      list_of_lines.append(line)
+    if return_as_string:
+      return '\n'.join(list_of_lines)
+    else:
+      return list_of_lines
         
   def print_dict_into_lines_then_trim_and_center(self, dictionary, 
-      length, keys_to_print = None, also_print_keys = False):
+      length, keys_to_print = None, also_print_keys = False,
+      return_as_string = False, allow_longer = False):
     r"""
-    Print some values of a dict, trimming and centering so that it
-    results in a box of characters.
+    Obtains lines from values from a dict, then trims and centers.
     
     Methods print_dict_into_lines, trim_to_single_line_string and
-    center_string are applied sequentially.
+    center_string are applied sequentially using the given arguments.
     """
-    pass
+    lines = self.print_dict_into_lines(
+        dictionary = dictionary,
+        keys_to_print = keys_to_print,
+        also_print_keys = also_print_keys,
+        return_as_string = False)
+    trimmed_lines = []
+    for line in lines:
+      trimmed_and_centered_line = self.trim_to_single_line_and_center_string(
+          line, length, allow_longer)
+      trimmed_and_centered_lines.append(trimmed_line)
+    if return_as_string:
+      return '\n'.join(trimmed_lines)
+    else:
+      return trimmed_lines
+      
+  def print_dict_into_box(self, dictionary, keys_to_print = None,
+      also_print_keys = False, return_as_string = False)
+    r"""
+    Produces smallest character box fitting values from a dict.
+    """
+    lines = self.print_dict_into_lines(
+        dictionary = dictionary,
+        keys_to_print = keys_to_print,
+        also_print_keys = also_print_keys,
+        return_as_string = False)
+    max_length = max(len(line) for line in lines)
+    return self.print_dict_into_lines_then_trim_and_center(
+        dictionary = dictionary,
+        length = max_length,
+        keys_to_print = keys_to_print,
+        also_print_keys = also_print_keys,
+        return_as_string = return_as_string,
+        allow_longer = False)
+        
