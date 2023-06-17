501f3f198b7f42da90d19b173a9e4c94d6147e1e
Eduardo Fischer <eduardofischermath@gmail.com>
2023-06-17 19:56:45 -0300

    (issue0025py) Develop initialization for StringBox. Slightly improved nearby methods.

--- src py/homemadefinancialinstruments/utilies/utilities.py
+++ src py/homemadefinancialinstruments/utilies/utilities.py
@@ -83,7 +83,9 @@ class StringManagement():
   
   def center_string(self, string, length, allow_longer = False):
     r"""
-    Centers a string into a specific length.
+    Returns a new string of a specific length where the original given
+    string appears in the center (or half character to the right,
+    if impossible to subdivide equally).
     
     If allow_longer is True, this acts exactly as str.center, so if the
     string is longer then it will return unaltered. Otherwise, if
@@ -92,7 +94,7 @@ class StringManagement():
     """
     if not allow_longer and len(string) > length:
       string = string[0:length]
-    return string.center()
+    return string.center(length)
 
   def trim_nonspace_whitespace_of_string(self, string,
       removing_instead_of_converting = False):
@@ -128,7 +130,7 @@ class StringManagement():
     applied sequentially.
     """
     trimmed_string = self.trim_nonspace_whitespace_of_string(
-        string = string
+        string = string,
         removing_instead_of_converting = removing_instead_of_converting)
     centered_trimmed_string = self.center_string(
         string = trimmed_string,
@@ -195,7 +197,7 @@ class StringManagement():
       return trimmed_lines
       
   def print_dict_into_string_box(self, dictionary, keys_to_print = None,
-      also_print_keys = False, return_as_string = False)
+      also_print_keys = False, return_as_string = False):
     r"""
     Produces smallest string box fitting values from a dict.
     
@@ -218,35 +220,88 @@ class StringManagement():
 
 class StringBox():
   r"""
-  List of lines of same size to be visualized in a pile
+  List of lines of same size to be visualized in a pile.
   
   The only attribute is list_of_lines, a list of strings (sometimes
   called lines) of the same size and without line breaks or other
   non-space whitespace characters.
   
-  An empty list (width 0, height 0) is allowed, as well as a nonempty
+  An empty list (width 0, height 0) is allowed, as well as any nonempty
   list of empty strings (width 0, nonzero height).
   """
   
-  def __init__(self, single_string = None, list_of_lines = None,
-      force_width_to = None, force_height_to = None, skip_checks = False):
-    if single_string is None and list_of_lines is None:
-      single_string = '' # Default value for initiation, thus
-    if single_string is not None and list_of_lines is not None:
-      # Check if both produce the same list_of_lines
-      ############
-      # WORK HERE
-      ############
-      raise ValueError('Init info must be given once not twice')
-    ############
-    # WORK HERE
-    ############
-  
+  def __init__(
+      self,
+      single_string = None,
+      list_of_lines = None,
+      force_width_to = None,
+      force_height_to = None,
+      align_to_center_instead_of_left = False,
+      skip_checks = False):
+    r"""
+    Initializes the instance with either a single string (which will be cut
+    through any of their line breaks) or a list of lines.
+    
+    The length of the resulting lines will be uniformized to the maximum
+    of the list, unless if force_width_to is set to a number, case in which
+    """
+    # Create instance of StringManagement class only if needed
+    if align_to_center_instead_of_left:
+      string_management = StringManagement()
+    # Read init arguments, get candidate_list_of_lines
+    if single_string is None:
+      if list_of_lines is None:
+        candidate_list_of_lines = [] # Default value for initiation, thus
+      else:
+        candidate_list_of_lines = list_of_lines
+    else:
+      if single_string == '':
+        # The default str.split('\n'), when called on the string '',
+        #produces the list [''], and not the empty list
+        # This might ou might not be expected but it is what it is,
+        #and will be manually set to be the correct behavior
+        candidate_list_of_lines = ['']
+      else:
+        candidate_list_of_lines = single_string.split('\n')
+      if list_of_lines is not None:
+        candidate_list_of_lines = single_string.split('\n')
+        if candidate_list_of_lines != list_of_lines:
+          raise ValueError('Init info must be given once not twice')
+      else:
+        candidate_list_of_lines = list_of_lines
+    if not skip_checks:
+      self.ensure_consistency_of_list_of_lines(candidate_list_of_lines)
+    # Now adjust length, height, and maybe center
+    if force_height_to is not None:
+      # Eliminate the bottom lines
+      if len(candidate_list_of_lines) > force_height_to:
+        candidate_list_of_lines = candidate_list_of_lines[:force_height_to]
+    if force_width_to is None:
+      correct_width = max(0, max(len(line) for line in candidate_list_of_lines))
+    else:
+      correct_width = force_width_to
+    correct_list_of_lines = []
+    for line in candidate_list_of_lines:
+      if len(line) > correct_width:
+        # Possible only if force_width_to is set to a number
+        correct_line = line[:correct_width]
+      else:
+        if align_to_center_instead_of_left:
+          correct_line = string_management.center_string(
+              string = line,
+              length = correct_width,
+              allow_longer = False)
+        else:
+          correct_line = line + ' '*(correct_width - len(line))
+      correct_list_of_lines.append(correct_line)
+    self.list_of_lines = correct_list_of_lines
+
   @classmethod
   def ensure_consistency_of_list_of_lines(cls, list_of_lines):
     r"""
     Ensures list of lines is a list made of strings of same size and
-    without special whitespace (only regular ASCII space is allowed).
+    without non-space whitespace (only regular ASCII space is allowed,
+    while '\t', '\n', '\r', '\x0b' and '\x0c' are forbidden).
     
     Returns None if everything is okay, and otherwise raises an Error.
     """
@@ -264,6 +319,9 @@ class StringBox():
   def get_width(self):
     list_of_lines = self.as_list_of_lines()
     if list_of_lines:
+      len(list_of_lines[0])
+    else:
+      return 0
     
   def get_height(self):
     list_of_lines = self.as_list_of_lines()
